7ac36d9e76becc2a682dd7288a88d5b7df83ca86
trytur <161700276+trytur@users.noreply.github.com>
2025-05-22 00:30:16 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	int N;
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		cout << i << '\n';
+}
